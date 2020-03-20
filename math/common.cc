e646656dd705175610d4af82463a4ed7a1b0b230
L. Nugent Gibson <lloyd.nugent.gibson@gmail.com>
2020-03-20 15:45:00 +0000

    changed floatEquals to templated function

--- math/common.cc
+++ math/common.cc
@@ -1,11 +1,3 @@
 #include "math/common.h"
 
-#include <cmath>
-
-namespace math {
-
-bool floatEquals(float a, float b, float tolerance) {
-  return fabs(a - b) < tolerance;
-}
-
-} // namespace math
+namespace math {} // namespace math
--- math/common.h
+++ math/common.h
@@ -1,9 +1,19 @@
 #ifndef MATH_COMMON_H
 #define MATH_COMMON_H
 
+#include <cmath>
+
 namespace math {
 
-bool floatEquals(float a, float b, float tolerance = .0001);
+template <typename T> bool floatEquals(T a, T b, T tolerance = .0001) {
+  if (std::isnan(a) && std::isnan(b)) {
+    return true;
+  }
+  if (std::isinf(a) && std::isinf(b)) {
+    return true;
+  }
+  return fabs(a - b) < tolerance;
+}
 
 } // namespace math
 
